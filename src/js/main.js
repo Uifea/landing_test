29b4dbb5883c6ed78330b8b205ccb339fd012e9b
Uifea <Evgenia-29@yandex.ru>
2023-10-18 17:58:01 +0300

    Update 2023-10-18T14:57:55.870Z

--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-let menuButton = document.querySelector('.header-page__humburger');
-let navList = document.querySelector('.site-list');
-
-menuButton.addEventListener('click', () => {
-    navList.classList.toggle('active');
-})
--- src/js/rating-number.js
+++
@@ -1,9 +0,0 @@
-let radioButtons = document.querySelectorAll('.rating__item');
-let ratingNumber = document.querySelector('.rating__number');
-
-
-radioButtons.forEach(item => {
-    item.addEventListener('click', () => {
-        ratingNumber.textContent = item.value
-    } )
-})
